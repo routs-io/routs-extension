ede68f466ea3e1bf10e799d9b6afea9cf3ab885b
Volodymyr Ivasiuk <ivasyukvolodimir@gmail.com>
2024-10-21 15:53:02 +0300

    added other transaction type to sign

--- src/components/TheSign.vue
+++ src/components/TheSign.vue
@@ -2,14 +2,15 @@
 import type { ITransaction } from '@/types/sign'
 import ApproveTx from './sign/ApproveTx.vue'
 import SwapTx from './sign/SwapTx.vue'
+import OtherTx from './sign/OtherTx.vue'
 import { useSignStore } from '@/stores/sign'
 import { toRefs } from 'vue'
 
 const { currentStep } = toRefs(useSignStore())
-const { signTransaction, addSignedStepToPath } = useSignStore();
+const { signTransaction, addSignedStepToPath } = useSignStore()
 
 const props = defineProps<{
-  id: number,
+  id: number
   address: string
   service: string
   activity: string
@@ -17,13 +18,13 @@ const props = defineProps<{
 }>()
 
 function cancel() {
-  currentStep.value = 0;
+  currentStep.value = 0
 }
 
 async function sign() {
-  const signedHash = await signTransaction(props.transaction);
-  console.log('signedHash', signedHash);
-  
+  const signedHash = await signTransaction(props.transaction)
+  console.log('signedHash', signedHash)
+
   addSignedStepToPath({
     id: props.id,
     address: props.address,
@@ -49,6 +50,13 @@ async function sign() {
   />
   <SwapTx
     v-else-if="activity === 'swap'"
+    :service="service"
+    :transaction="transaction"
+    @on-cancel="cancel"
+    @on-sign="sign"
+  />
+  <OtherTx
+    v-else
     :service="service"
     :transaction="transaction"
     @on-cancel="cancel"
