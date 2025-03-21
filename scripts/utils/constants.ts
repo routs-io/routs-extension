004f4076de78fc887745f2bf154e6bcc4b405aa0
Volodymyr Ivasiuk <ivasyukvolodimir@gmail.com>
2025-03-21 18:52:50 +0200

    fixed backend url

--- scripts/utils/constants.ts
+++ scripts/utils/constants.ts
@@ -1,8 +1,8 @@
 export const POPUP_WIDTH = 378
 export const POPUP_HEIGHT = 639
 
-//export const SOCKET_URL = 'wss://routs.dankov.co.uk'
-export const SOCKET_URL = 'ws://localhost:3000'
+export const SOCKET_URL = 'wss://routs.dankov.co.uk'
+//export const SOCKET_URL = 'ws://localhost:3000'
 
 export const SUPPORTED_POPUP_METHODS = ['navigate', 'eth_signTransactions', 'fuel_generateAccounts']
 
